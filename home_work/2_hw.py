4e8d3d617d1004e110e34d47a3ca63b01c99a4fd
Dmitriy <dimon798master@gmail.com>
2025-04-23 19:34:46 +0300

    First commit

---
+++ home_work/2_hw.py
@@ -0,0 +1,31 @@
+# a: int = 5
+# b: float = 15.001
+# c: str = 'строка'
+# d: list = [2, 4, 6, 8]
+# e: bool = True
+#
+# def rr(a: int, b: float, c: str, d: list, e: bool) -> str:
+#     return type(a)
+# print rr(5)
+# #
+#
+# print(a, "относится к типу", type(a))
+# print(b, "относится к типу", type(b))
+# print(c, "относится к типу", type(c))
+# print(d, "относится к типу", type(d))
+# print(e, "относится к типу", type(e))
+
+#
+# def sad(a = [1, 2, 3, 5, 8, 13, 21]):
+#     return sad(a)
+# print(a[0:3])
+
+
+def greet(a: int, b: float, c: str, d: list, e: bool) -> str:
+    return "Hello, " + type()
+a: int = 5
+b: float = 15.001
+c: str = 'строка'
+d: list = [2, 4, 6, 8]
+e: bool = True
+print(greet(c))
---
+++ python_training/task_1_numbers.py
@@ -0,0 +1,18 @@
+def ser():
+
+a = 2
+print(a, "относится к типу", type(a))
+
+b = 15.0001
+print(b, "относится к типу", type(b))
+
+c = 1+2j
+print(c, "комплексное чилсло", isinstance(c, complex))
+
+# def test(a=(1, 2, 3, 4)):
+#     return a[1]
+# print(test())
+#
+# def minlist(a: list, b: list) -> int:
+#     return max(len(a), len(b))
+# print(minlist([1, 2, 4, 5, 7], [1, 7, 7, 8, 8, 8, 8,7]))
