1d20b847d022b949b7b5cc4037b89516f1750b0d
Dmitriy <dimon798master@gmail.com>
2025-05-04 13:37:49 +0300

    First commit

---
+++ home_work/3_hw.py
@@ -0,0 +1,47 @@
+
+# EX2
+#
+#
+# #EX3
+# def func(x, y):
+#     if (x - y) == 135 or (y - x) == -135:
+#         print('yes')
+#     else:
+#         print('NO')
+# func(270, 137)
+#
+# #EX4
+# def sezon(month):
+#     if month==12 or month==1 or month==2:
+#         print('Зима')
+#     elif month==3 or month==4 or month==5:
+#         print('Весна')
+#     elif month==6 or month==7 or month==8:
+#         print('Лето')
+#     elif month==9 or month==10 or month==11:
+#         print('Осень')
+#     else:
+#         print('Введите месяц от 1 до 12')
+# sezon(42)
+#
+#
+# #EX5
+# def fanc(x, y, z):
+#     if x>10 and y>10 and z>10:
+#         print('yes')
+#     else:
+#         print('no')
+# fanc(20,4,56)
+#
+#
+# #Ex6
+# l = [2,4,-7,-9,6,-4,0,7,-1,2]
+# def f(e):
+#     return e > 0
+# print(len(list(filter(f,l))))
+#
+#
+# EX7
+# def funk(a: int, b: int) -> int:
+#     return a*12*29+b*29
+# print(funk(3, 1))
---
+++ home_work/5_hw.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+## Поиск элемента
+username = driver.find_element(By.CSS_SELECTOR, '[id="user-name"]')
+
+if username is None:
+    print('Элемент user-name не найден')
+else:
+    print('Элемент user-name найден')
+
+password = driver.find_element(By.CSS_SELECTOR, '#password')
+
+if password is None:
+    print('Элемент password не найден')
+else:
+    print('Элемент password найден')
+
+submit = driver.find_element(By.CSS_SELECTOR, '[class="submit-button btn_action"]')
+
+if submit is None:
+    print('Элемент submit не найден')
+else:
+    print('Элемент submit найден')
