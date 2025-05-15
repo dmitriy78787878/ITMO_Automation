5926428fe058db49ce0ce7d283067c2147d9ef39
Dmitriy <dimon798master@gmail.com>
2025-05-15 23:44:17 +0300

    1

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,28 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
+def test_5_open():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
 
+    ## Поиск элемента
+    username = driver.find_element(By.CSS_SELECTOR, '[id="user-name"]')
+    if username is None:
+        print('Элемент не найден')
+    else:
+        print('Элемент найден')
 
-## Поиск элемента
-username = driver.find_element(By.CSS_SELECTOR, '[id="user-name"]')
+    password = driver.find_element(By.CSS_SELECTOR, '#password')
 
-if username is None:
-    print('Элемент user-name не найден')
-else:
-    print('Элемент user-name найден')
+    if password is None:
+        print('Элемент password не найден')
+    else:
+        print('Элемент password найден')
 
-password = driver.find_element(By.CSS_SELECTOR, '#password')
+    submit = driver.find_element(By.CSS_SELECTOR, '[class="submit-button btn_action"]')
 
-if password is None:
-    print('Элемент password не найден')
-else:
-    print('Элемент password найден')
-
-submit = driver.find_element(By.CSS_SELECTOR, '[class="submit-button btn_action"]')
-
-if submit is None:
-    print('Элемент submit не найден')
-else:
-    print('Элемент submit найден')
+    if submit is None:
+        print('Элемент submit не найден')
+    else:
+        print('Элемент submit найден')
