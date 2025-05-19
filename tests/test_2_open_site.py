85abc98913eebff604fd4b69fa0270fac87fe977
Dmitriy <dimon798master@gmail.com>
2025-05-19 21:34:06 +0300

    7

--- tests/test_2_open_site.py
+++ tests/test_2_open_site.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://demoqa.com/")
+def test_2_open():
+    driver = webdriver.Chrome()
+    driver.get("https://demoqa.com/")
 
-## Поиск элемента
-icon = driver.find_element(By.CSS_SELECTOR, 'header > a > img')
-if icon is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
+    ## Поиск элемента
+    icon = driver.find_element(By.CSS_SELECTOR, 'header > a > img')
+    if icon is None:
+        print('Элемент не найден')
+    else:
+        print('Элемент найден')
+
+    password = driver.find_element(By.CSS_SELECTOR, '#password')
+    if password is None:
+        print('Элемент password не найден')
+    else:
+        print('Элемент password найден')
+
